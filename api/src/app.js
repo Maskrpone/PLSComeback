b256b54e88cbd0bd7dd9056900e5abeca7783b48
Maskrpone <90637175+Maskrpone@users.noreply.github.com>
2024-01-22 14:54:12 +0100

    Commit for pull

--- api/src/app.js
+++ api/src/app.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 const port = 3000;
+
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
+// Define a simple mongoose model and schema
+const Todo = mongoose.model("Todo", { text: String });
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
