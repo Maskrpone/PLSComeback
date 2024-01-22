2fb675f53ae434e202723185dfb4757057a2607e
Slyvred <remihage2003@gmail.com>
2024-01-22 19:54:24 +0100

    supression de test.js, code déplacé dans routes/users.js

--- api/app.js
+++ api/app.js
@@ -6,7 +6,6 @@ let logger = require('morgan');
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
-let testRouter = require('./routes/test');
 
 let mongoose = require('mongoose')
 let app = express();
@@ -32,7 +31,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/test', testRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,16 @@
 let express = require('express');
 let router = express.Router();
+let Users = require('../models/users');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('test');
+// Route pour obtenir tous les utilisateurs
+router.get('/', async (req, res) => {
+  try {
+    const users = await Users.find();
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 module.exports = router;
