65c43c980469ebf024b8d68b97030bd3ea31df5d
Maskrpone <90637175+Maskrpone@users.noreply.github.com>
2024-01-22 12:08:03 +0100

    Express template

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # PLSComeback
+
+_This is the BackEnd branch of a small IT school project_
+
--- api/src/app.js
+++ api/src/app.js
@@ -1,13 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Add a new route for GET requests to /another
+app.get("/another", (req, res) => {
+  res.send("This is another route!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-
+  console.log(`Example app listening on port ${port}`);
+});
