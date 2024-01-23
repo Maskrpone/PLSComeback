4c3653dfa9ffde0cd555741ade05e493dba15ec3
Maskrpone <90637175+Maskrpone@users.noreply.github.com>
2024-01-23 16:50:31 +0100

    supplies update

--- api/routes/supplies.js
+++ api/routes/supplies.js
@@ -15,8 +15,7 @@ router.get("/", async (req, res) => {
 router.get("/:name", async (req, res) => {
   try {
     const supply = await Supplies.findOne({ name: req.params["name"] });
-    //res.json(supply);
-    (supply == null) ? res.status(404).send() : res.json(supply);
+    supply == null ? res.status(404).send() : res.json(supply);
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
@@ -26,21 +25,13 @@ router.get("/:name", async (req, res) => {
 // Créer un nouveau consommable
 router.post("/", async (req, res) => {
   try {
-    const newSupply = new Supplies(req.body);
-    const savedSupply = await newSupply.save();
-    res
-      .status(200)
-      .json({ message: "Supply added successfully", supply: savedSupply });
+    const nouvelleFourniture = new Supplies(req.body);
+    const fournitureEnregistree = await nouvelleFourniture.save();
+    res.status(201).json(fournitureEnregistree);
   } catch (error) {
     console.error(error);
-    res
-      .status(500)
-      .send(
-        "Internal server error, your data is structured as : {'name': ?(String), 'quantity': ?(Number)}",
-      );
+    res.status(500).json({ message: "Erreur Serveur" });
   }
 });
-
-// ajouter une quantité au
 
 module.exports = router;
