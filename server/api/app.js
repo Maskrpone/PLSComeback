f618a1833dba9a878795b2b73da73b390fac95ab
Maskrpone <90637175+Maskrpone@users.noreply.github.com>
2024-01-24 18:42:11 +0100

    CORS errors should be disabled now

--- server/api/app.js
+++ server/api/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -37,6 +38,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors);
 
 // routing
 app.use("/", indexRouter);
