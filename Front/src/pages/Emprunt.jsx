0afe53d0dc1b15eca481abee08ff0499630af7f6
Slyvred <remihage2003@gmail.com>
2024-01-24 21:58:02 +0100

    intégration de l'api dans la page d'emprunt

--- Front/src/pages/Emprunt.jsx
+++ Front/src/pages/Emprunt.jsx
@@ -1,58 +1,122 @@
 import { Link } from "react-router-dom";
-import "./Emprunt.css"
-import {Footer,Header} from "./Components/HeadFoot"
+import "./Emprunt.css";
+import { Footer, Header } from "./Components/HeadFoot";
+
+import axios from "axios";
+import qs from "qs";
+import { API_IP } from "../Constants";
 
 import BoutonRetour from "./Components/BoutonRetour";
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 
 function Emprunt() {
-    const pictures = [
-        { title: 'Perceuse', url: 'https://cdn.toolstation.fr/images/141021-FR/250/16045.jpg' },
-        { title: 'Perceuse 2', url: 'https://cdn.toolstation.fr/images/141021-FR/250/19793.jpg' },
-        { title: 'Perceuse 3', url: 'https://cdn.toolstation.fr/images/141021-FR/250/87199.jpg' },
-        { title: 'Perceuse 4', url: 'https://cdn.toolstation.fr/images/141021-FR/250/24035.jpg' },
-        { title: 'Perceuse 5', url: 'https://cdn.toolstation.fr/images/141021-FR/250/19794.jpg' },
-        { title: 'Perceuse 6', url: 'https://cdn.toolstation.fr/images/141021-FR/250/19794.jpg' },
-        { title: 'Perceuse 7', url: 'https://cdn.toolstation.fr/images/141021-FR/250/19794.jpg' },
-        { title: 'Perceuse 8', url: 'https://cdn.toolstation.fr/images/141021-FR/250/19794.jpg' },
-        { title: 'Perceuse 9', url: 'https://cdn.toolstation.fr/images/141021-FR/250/19794.jpg' },
-        { title: 'Perceuse 10', url: 'https://cdn.toolstation.fr/images/141021-FR/250/19794.jpg' },
-        { title: 'Perceuse 11', url: 'https://cdn.toolstation.fr/images/141021-FR/250/19794.jpg' },
-        { title: 'Perceuse 12', url: 'https://cdn.toolstation.fr/images/141021-FR/250/19794.jpg' },
-        { title: 'Perceuse 13', url: 'https://cdn.toolstation.fr/images/141021-FR/250/19794.jpg' },
+	const [pictures, setPictures] = useState([]);
 
-    ]
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await axios.get(`http://${API_IP}:3000/tools`);
 
-    return (
-        <>
-            <Header/>
-            <header>
-                <h1> Tool rental </h1>
-            </header>
+				const formattedData = response.data.map((tool) => ({
+					title: tool.name,
+					url: tool.image,
+				}));
 
-            <BoutonRetour url="/"/>
+				console.log(formattedData);
 
-            <div id="wrapper">
-                {pictures.map((picture, index) => (
-                    <div key={index} className="card" >
-                        <img src={picture.url} alt={picture.title} />
-                        {/* <quote>Stock:</quote> */}
-                        <>{picture.title}</>
+				setPictures(formattedData);
+			} catch (error) {
+				console.log(error);
+			}
+		};
 
-                        {/* <button class="learn-more">
+		fetchData();
+	}, []); // Empty dependency array ensures the effect runs once when the component mounts
+
+    /*
+	const pictures = [
+		{
+			title: "Perceuse",
+			url: "https://cdn.toolstation.fr/images/141021-FR/250/16045.jpg",
+		},
+		{
+			title: "Perceuse 2",
+			url: "https://cdn.toolstation.fr/images/141021-FR/250/19793.jpg",
+		},
+		{
+			title: "Perceuse 3",
+			url: "https://cdn.toolstation.fr/images/141021-FR/250/87199.jpg",
+		},
+		{
+			title: "Perceuse 4",
+			url: "https://cdn.toolstation.fr/images/141021-FR/250/24035.jpg",
+		},
+		{
+			title: "Perceuse 5",
+			url: "https://cdn.toolstation.fr/images/141021-FR/250/19794.jpg",
+		},
+		{
+			title: "Perceuse 6",
+			url: "https://cdn.toolstation.fr/images/141021-FR/250/19794.jpg",
+		},
+		{
+			title: "Perceuse 7",
+			url: "https://cdn.toolstation.fr/images/141021-FR/250/19794.jpg",
+		},
+		{
+			title: "Perceuse 8",
+			url: "https://cdn.toolstation.fr/images/141021-FR/250/19794.jpg",
+		},
+		{
+			title: "Perceuse 9",
+			url: "https://cdn.toolstation.fr/images/141021-FR/250/19794.jpg",
+		},
+		{
+			title: "Perceuse 10",
+			url: "https://cdn.toolstation.fr/images/141021-FR/250/19794.jpg",
+		},
+		{
+			title: "Perceuse 11",
+			url: "https://cdn.toolstation.fr/images/141021-FR/250/19794.jpg",
+		},
+		{
+			title: "Perceuse 12",
+			url: "https://cdn.toolstation.fr/images/141021-FR/250/19794.jpg",
+		},
+		{
+			title: "Perceuse 13",
+			url: "https://cdn.toolstation.fr/images/141021-FR/250/19794.jpg",
+		},
+	];*/
+
+	return (
+		<>
+			<Header />
+			<header>
+				<h1> Tool rental </h1>
+			</header>
+
+			<BoutonRetour url="/" />
+
+			<div id="wrapper">
+				{pictures.map((picture, index) => (
+					<div key={index} className="card">
+						<img src={picture.url} alt={picture.title} />
+						{/* <quote>Stock:</quote> */}
+						<>{picture.title}</>
+
+						{/* <button class="learn-more">
                         <span class="circle" aria-hidden="true">
                         <span class="icon arrow"></span>
                         </span>
                             <span class="button-text">Emprunter</span>
                         </button> */}
-                    </div>
-                ))}
-
-            </div>
-            <Footer/>
-        </>
-    )
+					</div>
+				))}
+			</div>
+			<Footer />
+		</>
+	);
 }
 
 export default Emprunt;
