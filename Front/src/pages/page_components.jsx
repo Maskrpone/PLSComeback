7fad2896f15f233e061adc7a0e6fbfd7838dbf3f
HugoMany <90179566+HugoMany@users.noreply.github.com>
2024-01-25 08:56:47 +0100

    Push

--- Front/src/pages/page_components.jsx
+++ Front/src/pages/page_components.jsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import React, { useState, useEffect } from "react";
 import "./../App.css";
 import { Link } from "react-router-dom";
@@ -32,18 +31,6 @@ const Page_components = () => {
     
         fetchData();
       }, []);
-=======
-import React, { useState } from 'react';
-import "./../App.css";
-import { Link } from "react-router-dom";
-import { Footer, Header } from "./Components/HeadFoot"
-import BoutonRetour from './Components/BoutonRetour';
-import axios from 'axios';
-
-
-const Page_components = () => {
-    let admin = true;
->>>>>>> Stashed changes
 
 	const admin = true;
 
@@ -57,7 +44,6 @@ const Page_components = () => {
 		return { ...user, taille: user.taille - 1 };
 	};
 
-<<<<<<< Updated upstream
 	// const [users, setUsers] = useState([
 	// 	new User(
 	// 		"John Doe",
@@ -75,27 +61,6 @@ const Page_components = () => {
 	// 		185,
 	// 	),
 	// ]);
-=======
-    const handleClick = (str) => {
-        // Données à envoyer
-        var dataToSend = {
-            key1: 'valeur1',
-            key2: 'valeur2'
-        };
-
-        // URL de l'API
-        var apiUrl = 'http://10.224.1.231:3000/supplies/';
-
-        axios.get(apiUrl)
-            .then(function (response) {
-                // Traitement des données récupérées
-                console.log('Succès de la requête GET :', response.data);
-            })
-            .catch(function (error) {
-                console.error('Erreur lors de la requête GET :', error);
-            });
-    };
->>>>>>> Stashed changes
 
 	const handleClick = (str) => {
 		console.log(str);
@@ -105,7 +70,6 @@ const Page_components = () => {
 		user.nom.toLowerCase().includes(searchTerm.toLowerCase()),
 	);
 
-<<<<<<< Updated upstream
 	return (
 		<div className="App">
 			<Header />
@@ -250,121 +214,6 @@ const Page_components = () => {
 		</div>
 	);
 };
-=======
-    return (
-        <div className="App">
-            <Header />
-            <header className="App-header">
-                <BoutonRetour url="/" />
-                <Link to="/"> home</Link>
-                <br /><br />
-                <div className="w-96 mb-3 xl:w-96">
-                    <div className="relative mb-4 flex w-full flex-wrap items-stretch">
-                        <input
-                            type="search"
-                            className="relative m-0 block flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-200 outline-none transition duration-300 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-400 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
-                            placeholder="Search"
-                            aria-label="Search"
-                            aria-describedby="button-addon2"
-                            value={searchTerm}
-                            onChange={(e) => setSearchTerm(e.target.value)}
-                        />
-
-                        {/* <!--Search icon--> */}
-                        <span
-                            className=" input-group-text flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center text-base font-normal text-neutral-300 dark:text-neutral-300"
-                            id="basic-addon2">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                className="h-5 w-5">
-                                <path
-                                    fillRule="evenodd"
-                                    d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-                                    clipRule="evenodd" />
-                            </svg>
-                        </span>
-                    </div>
-                </div>
-                <div className="lg:w-3/4">
-                    <table
-                        className="min-w-96 lg:w-full mx-auto w-16 md:w-32 border-amber-100 border-solid border-2 rounded-xl overflow-auto">
-                        <thead className="min-w-80 outline outline-amber-100">
-                            <tr className="flex lg:w-11/12 center mb-4 ml-12 mr-10 lg:flex-none">
-
-                                <th className="flex float-left lg:w-1/3 lg:float-none lg:flex">Image</th>
-
-                                <div className="flex  w-full float-right pl-36 lg:w-9/12 lg:pl-0 lg:flex-none lg:justify-start">
-
-                                    <th className="flex float-right lg:w-1/3 lg:justify-center lg:float-none lg:flex-auto">Name </th>
-                                    <th className="lg:hidden">/</th>
-                                    <th className="flex float-right lg:w-1/3 lg:float-none lg:justify-end lg:flex-auto">Stock</th>
-
-                                </div>
-
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {filteredUsers.length === 0 ? (
-                                <tr>
-                                    <td colSpan="3">No result</td>
-                                </tr>
-                            ) : (
-                                filteredUsers.map((user, index) => (
-                                    <tr key={index} className="block mb-4 lg:flex lg:w-full">
-                                        <td className="block float-left lg:flex-auto lg:w-1/3">
-                                            <img className="max-w-36 m-3" src={user.image} alt={'img of ' + user.nom} />
-                                        </td>
-                                        <td className="block text-right lg:flex-auto lg:text-center lg:w-1/3">
-                                            <p className="min-w-96 mr-5 lg:min-w-0 lg:mr-0 "><br />{user.nom}</p>
-                                        </td>
-                                        <td className="lg:justify-center block text-right lg:flex-auto lg:w-1/3">
-                                            <p className="min-w-96 lg:min-w-0">
-                                                {admin === true ?
-                                                    (<button onClick={() => setUsers(prevUsers => {
-                                                        const updatedUsers = [...prevUsers];
-                                                        updatedUsers[index] = incrementTaille(user);
-                                                        return updatedUsers;
-                                                    })} className="bg-amber-500 rounded-full min-w-8 text-center m-3"> +
-                                                    </button>
-                                                    ) : ('')}
-
-
-                                                {user.taille}
-
-                                                {admin === true ?
-                                                    (<button onClick={() => setUsers(prevUsers => {
-                                                        const updatedUsers = [...prevUsers];
-                                                        updatedUsers[index] = decrementTaille(user);
-                                                        return updatedUsers;
-                                                    })} className="bg-amber-500 rounded-full min-w-8 text-center m-3"> -
-                                                    </button>
-                                                    ) : ('')}
-                                            </p>
-
-                                        </td>
-                                    </tr>
-                                ))
-                            )}
-                        </tbody>
-                    </table>
-                    {admin === true ?
-                        (
-                            <button onClick={() => handleClick(users)}
-                                className="w-full lg:relative lg:float-right lg:w-60 bg-amber-500 border-4 border-amber-500 rounded-xl mt-4">Update
-                                stock
-                            </button>
-                        ) : ('')}
-                </div>
-                <br /><br />
-            </header>
-            <Footer />
-        </div>
-
-    );
-}
->>>>>>> Stashed changes
 
 function User(nom, image, taille) {
 	this.nom = nom;
